cc74c64fc92b00366fdd9e7ed395f13d12557a85
Luan Blakaj <luan.b@rankit.online>
2019-01-25 01:28:00 +0100

    Details

--- src/pages/warmup/warmup.ts
+++ src/pages/warmup/warmup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, Card, Item, CardContent, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the WarmupPage page.
