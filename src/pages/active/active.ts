91a2bde9d6b9bbfcaf4b3eeb4e3705323dae9fb7
luanblakaj <luan.b@rankit.online>
2019-03-05 14:08:34 +0100

    Idk

--- src/pages/active/active.ts
+++ src/pages/active/active.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
 import { TrainerDetailPage } from '../trainer-detail/trainer-detail'
 
-/**
- * Generated class for the ActivePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-active',
